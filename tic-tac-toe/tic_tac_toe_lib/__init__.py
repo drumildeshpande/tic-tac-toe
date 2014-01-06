757406abcfc199b41c3f49b8c4ebb38ca58456f1
drumil <drumil@dvd.(none)>
2014-01-06 20:23:32 +0530

    first commit

---
+++ tic-tac-toe/tic_tac_toe_lib/__init__.py
@@ -0,0 +1,12 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+'''facade - makes tic_tac_toe_lib package easy to refactor
+
+while keeping its api constant'''
+from . helpers import set_up_logging
+from . Window import Window
+from . tic_tac_toeconfig import get_version
+
